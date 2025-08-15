813758125f656a8064d932d381977797dcbab99b
Winston Lin <winstonnlinn@gmail.com>
2025-08-14 22:56:54 -0400

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className= "fixed bottom-0 w-full mb-0 py-2">
             <p className = "text-xs text-black-500 text-center px-4">
-                @ {new Date().getFullYear()} All rights reserved.
+                @ {new Date().getFullYear()} All rights reserved..
             </p>
         </footer>
     )

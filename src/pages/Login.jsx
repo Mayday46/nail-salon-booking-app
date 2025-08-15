7295de61fb0e31a67c8c0c30c5df023a9399e7ee
Mayday46 <winstonnlinn@gmail.com>
2025-08-14 22:46:05 -0400

    fix: correct header text on login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
         <div className = "flex items-center justify-center min-h-screen bg-gray-100">
             <div className ="w-full max-w-sm p-6 bg-white rounded-2xl shadow-lg" >
                 <h2 className = "mb-6 text-2xl font-bold text-center">
-                    Login Page..
+                    Login Page
                 </h2>
 
                 <form className = "space-y-4">
